13ef17a181ef39731361c9556a03a43ca117669d
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-02-06 09:29:17 +0530

    implementing frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import LoginPage from './pages/auth/login/LoginPage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import HomePage from './pages/home/HomePage'
+import Sidebar from './components/common/Sidebar'
+import RightPanel from './components/common/RightPanel'
+
 export default function App() {
   return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+      <RightPanel />
     </div>
   )
 }
