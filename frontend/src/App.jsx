32d29cd5ea10e5655a220be0603c05250377e1a8
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-02-06 21:44:01 +0530

    implementing ui/ux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,11 @@ import SignUpPage from './pages/auth/signup/SignUpPage'
 import HomePage from './pages/home/HomePage'
 import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 
-export default function App() {
+
+export default function App() {        
   return (
     <div className='flex max-w-6xl mx-auto'>
       <Sidebar />
@@ -15,6 +18,8 @@ export default function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
       </Routes>
       <RightPanel />
     </div>
