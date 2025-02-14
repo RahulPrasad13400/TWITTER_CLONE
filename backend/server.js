93950150bd54fd73907a1bc4e0ea986747ebd112
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-02-14 21:54:29 +0530

    prepared the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"          // step 1 (env) ee rand step cheyuthale env file il ninn data access cheyyan patullu
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
@@ -16,6 +17,7 @@ app.use(cookieParser()) // to get cookie
 app.use(express.urlencoded({extended : true}))       // to parase data from urlencoded (postman)
 
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 dotenv.config() // step 2 (env) ee rand step cheyuthale env file il ninn data access cheyyan patullu
 
@@ -30,6 +32,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
+
+if(process.env.NODE_ENV.trim() === "production"){
+    console.log("hello")
+        app.use(express.static(path.join(__dirname, "/frontend/dist")))
+        app.get("*", (req, res)=>{
+            res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+        })
+}
 
 app.listen(PORT,()=>{ 
     console.log("server is running on port",PORT)
